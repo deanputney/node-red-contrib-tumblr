3474df775a5add42a3fce92d4aa51a5e4f678761
JJ Cantillon <jjcantillon@gmail.com>
2018-01-05 20:52:54 +1000

    fix js errors

--- locales/en-US/tumblr.js
+++ locales/en-US/tumblr.js
@@ -1,7 +1,9 @@
 {
   "tumblr": {
     "label": {
-      "clickhere": "Click here to authenticate with Tumblr."
+      "clickhere": "Click here to authenticate with Tumblr.",
+      "tumblr-id": "Tumblr ID",
+      "for": "for"
     }
   }
 }
